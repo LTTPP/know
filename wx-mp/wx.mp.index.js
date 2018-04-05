a1bf5585eebf2f0233801ed01b8fa5941a2fb2ed
jianliuwei <liujianwei.me@gmail.com>
2018-04-05 22:58:53 +0800

    use thunkify

--- wx-mp/wx.mp.index.js
+++ wx-mp/wx.mp.index.js
@@ -3,6 +3,7 @@
 const ali = require('../alicloud/ali.services.tobase64.client');
 const baidu = require('../baidu-aip/baidu.services.imgrecog.client');
 const co = require('co');
+const thunkify = require('thunkify');
 
 const objectKey = 'sample.jpg';
 
@@ -10,8 +11,8 @@ const objectKey = 'sample.jpg';
 ali.put(objectKey, '../assets/' + objectKey);
 
 // Convert file to base64 string and recognize
-ali.tobase64(objectKey, function (b64str) {
-    baidu.recognize(b64str, function (result) {
+thunkify(ali.tobase64)(objectKey)(function (b64str) {
+    thunkify(baidu.recognize)(b64str)(function (result) {
         console.log(result);
     });
 });
